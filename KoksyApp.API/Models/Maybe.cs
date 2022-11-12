f41146d4fbbc580ae78ee1f2c472d992ba805977
Patrycja <psu@norlysenergytrading.com>
2022-11-12 17:18:05 +0100

    test

---
+++ KoksyApp.API/Models/Maybe.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+
+namespace KoksyApp.API.Models;
+
+public abstract class Maybe<T>
+{
+    public static Maybe<T> Of(T value)
+    {
+        return value == null?   new None<T>() :new Some<T>(value);
+    }
+    public abstract Maybe<T1> Map<T1>(Func<T, T1> f);
+    public abstract T GetValueOrFallback(T fallback);
+
+}
+public class None<T> : Maybe<T>
+{
+    public override Maybe<T1> Map<T1>(Func<T, T1> f)
+    {
+        return new None<T1>();
+    }
+
+    public override T GetValueOrFallback(T fallback)
+    {
+        return fallback;
+    }
+}
+
+public class Some<T> : Maybe<T>
+{
+    public readonly T Value;
+    public Some(T value) => this.Value = value;
+
+    public override Maybe<T1> Map<T1>(Func<T, T1> f)
+    {
+        return new Some<T1>(f(Value));
+    }
+
+    public override T GetValueOrFallback(T fallback)
+    {
+        return Value;
+    }
+}
---
+++ KoksyApp.API/Repositories/UserDayRepository.cs
@@ -0,0 +1,51 @@
+﻿using KoksyApp.API.Models;
+using MongoDB.Driver;
+
+namespace KoksyApp.API.Repositories;
+
+public interface IUserDayRepository
+{
+
+    Task<bool> Add(string dayId, string userId);
+    Task<UserDay[]> GetForUser(string userId);
+    Task<UserDay> GetUserDay(string userId, string dayId);
+    Task<UserDay> Update(string dayId, string userId, DateTime dateTime);
+}
+public class UserDayRepository :BaseRepository<UserDay>, IUserDayRepository
+{
+    private readonly IMongoDbClient mongoDbClient;
+
+    public UserDayRepository(IMongoDbClient mongoDbClient) : base(mongoDbClient)
+    {
+        this.mongoDbClient = mongoDbClient;
+    }
+
+
+    public Task<bool> Add(string dayId, string userId)
+    {
+        GetCollection().InsertOne(new UserDay()
+        {
+            DayId = dayId,
+            UserId = userId,
+            LastOpened = DateTime.Now,
+        });
+        return  GetCollection().Find(x => x.UserId == userId && x.DayId == dayId).AnyAsync();
+    }
+
+    public async Task<UserDay[]> GetForUser(string userId)
+    {
+        return  GetCollection().Find(x => x.UserId == userId).ToEnumerable().ToArray();
+    }
+
+    public async Task<UserDay> GetUserDay(string userId, string dayId)
+    {
+        return  GetCollection().Find(x => x.UserId == userId && x.DayId == dayId).FirstOrDefault();
+    }
+
+    public Task<UserDay> Update(string dayId, string userId, DateTime dateTime)
+    {
+        var updatedef = Builders<UserDay>.Update
+            .Set(x => x.LastOpened, dateTime);
+      return  GetCollection().FindOneAndUpdateAsync(x => x.UserId == userId && x.DayId == dayId, updatedef);
+    }
+}
